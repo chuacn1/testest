695b70f6b188e6b991e05dad74fdc161945b6674
Clarissa Chua (1000126504) <CHUACN1@student.op.ac.nz>
2024-09-26 15:24:03 +1200

    added C# files

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Testest
+{
+    internal class Program
+    {
+
+        public static void Aareena()
+        {
+
+        }
+
+        public static void Clarissa()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Aareena();
+            Clarissa();
+        }
+    }
+}
