08f16f4bca044b0ae6106d7af696decc1ca6b2b5
Clarissa Chua (1000126504) <CHUACN1@student.op.ac.nz>
2024-09-26 15:33:08 +1200

    guess this bro

--- Program.cs
+++ Program.cs
@@ -10,8 +10,32 @@
 
         public static void Clarissa()
         {
+            string tempt;
+            int number;
 
+
+            while (true)
+            {
+                Console.WriteLine("Guess a number between 1 to 3");
+                tempt = Console.ReadLine();
+                number = Convert.ToInt32(tempt);
+
+                if (number == 3)
+                {
+                    Console.WriteLine("Very smart");
+                }
+
+                else
+                {
+                    Console.WriteLine("WROOONNNG");
+
+                    Console.WriteLine("");
+                }
+
+            }
         }
+
+
         static void Main(string[] args)
         {
             Aareena();
